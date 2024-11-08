e5436e4e146738c53372e8b5571c115e3f8c42d0
Th. Androutsos <a8anassis@gmail.com>
2024-11-08 16:54:11 +0200

    updates

--- GradesApp/Program.cs
+++ GradesApp/Program.cs
@@ -16,45 +16,70 @@
             int average = 0;
 
             Console.WriteLine("Please enter grades sum and grades count");
-            if (!int.TryParse(Console.ReadLine()!, out int gradesSum)) {
+            if (!int.TryParse(Console.ReadLine()!, out int totalMarks)) {
                 Console.WriteLine("Error in read");
             }
 
-            if (gradesSum <= 0)
+            if (totalMarks <= 0)
             {
                 Environment.Exit(1);
             }
 
-            if (!int.TryParse(Console.ReadLine()!, out int gradesCount))
+            if (!int.TryParse(Console.ReadLine()!, out int coursesCount))
             {
                 Console.WriteLine("Error in read");
             }
 
 
-            if (gradesCount <= 0)
+            if (coursesCount <= 0)
             {
                 Console.WriteLine("Grades count can not be negative or zero");
                 Environment.Exit(1);
             }
 
-            average = gradesSum / gradesCount;
+            average = totalMarks / coursesCount;
 
-            if (average >= 8)
+            if (average > 10)
             {
-                Console.WriteLine($"Excellent: {average:N2}");
+                Console.WriteLine("Average must not be greater than 10");
+                Environment.Exit(1);
             }
-            else if (average >= 6)
+
+            switch (average)
             {
-                Console.WriteLine($"Very Good: {average:N2}");
+                case >= 8:
+                    Console.WriteLine($"Excellent: {average:N2}");
+                    break;
+
+                case >= 6:
+                    Console.WriteLine($"Very Good: {average:N2}");
+                    break;
+
+                case >= 5:
+                    Console.WriteLine($"Good: {average:N2}");
+                    break;
+
+                default:
+                    Console.WriteLine($"Failure: {average:N2}");
+                    break;
             }
-            else if (average >= 5)
-            {
-                Console.WriteLine($"Good: {average:N2}");
-            }
-            else
-            {
-                Console.WriteLine($"Failure: {average:N2}");
-            }
+
+            //if (average >= 8)
+            //{
+            //    Console.WriteLine($"Excellent: {average:N2}");
+            //}
+            //else if (average >= 6)
+            //{
+            //    Console.WriteLine($"Very Good: {average:N2}");
+            //}
+            //else if (average >= 5)
+            //{
+            //    Console.WriteLine($"Good: {average:N2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Failure: {average:N2}");
+            //}
         }
     }
 }
